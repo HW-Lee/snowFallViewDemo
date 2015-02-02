0f660c764f827623af4e25062e799b5ebe77f179
HW-Lee <tony123930@gmail.com>
2015-02-03 03:57:49 +0800

    first demo

--- src/com/hwlee/snowfallviewdmo/SnowFallView.java
+++ src/com/hwlee/snowfallviewdmo/SnowFallView.java
@@ -7,46 +7,59 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.*;
 import android.view.animation.Animation;
 import android.view.animation.Interpolator;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.TranslateAnimation;
+import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
 
-public class SnowFallView extends View {
+public class SnowFallView extends View implements OnTouchListener {
 
+	private Context parentContext;
 	private int snow_flake_count = 10;
 	private final ArrayList<Drawable> drawables = new ArrayList<Drawable>();
 	private int[][] coords;
 	private final Drawable snow_flake;
+	private boolean flag = true;
 
 	public SnowFallView(Context context) {
 		super(context);
 		// TODO Auto-generated constructor stub
-		snow_flake = null;
+		snow_flake = context.getResources().getDrawable(R.drawable.snowflake);
 		snow_flake.setBounds(0, 0, snow_flake.getIntrinsicWidth(),
 				snow_flake.getIntrinsicHeight());
+		setOnTouchListener(this);
+		parentContext = context;
 	}
 
 	public SnowFallView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		// TODO Auto-generated constructor stub
-		snow_flake = null;
+		snow_flake = context.getResources().getDrawable(R.drawable.snowflake);
 		snow_flake.setBounds(0, 0, snow_flake.getIntrinsicWidth(),
 				snow_flake.getIntrinsicHeight());
+		setOnTouchListener(this);
+		parentContext = context;
 	}
 
 	public SnowFallView(Context context, AttributeSet attrs, int defStyleAttr) {
 		super(context, attrs, defStyleAttr);
 		// TODO Auto-generated constructor stub
-		snow_flake = null;
+		snow_flake = context.getResources().getDrawable(R.drawable.snowflake);
 		snow_flake.setBounds(0, 0, snow_flake.getIntrinsicWidth(),
 				snow_flake.getIntrinsicHeight());
+		setOnTouchListener(this);
+		parentContext = context;
 	}
 
 	@Override
 	protected void onSizeChanged(int width, int height, int oldw, int oldh) {
 		super.onSizeChanged(width, height, oldw, oldh);
+		if (!flag) return;
 		Random random = new Random();
 		Interpolator interpolator = new LinearInterpolator();
 
@@ -71,6 +84,7 @@ public class SnowFallView extends View {
 
 	@Override
 	protected void onDraw(Canvas canvas) {
+		if (!flag) return;
 		for (int i = 0; i < snow_flake_count; i++) {
 			Drawable drawable = drawables.get(i);
 			canvas.save();
@@ -79,6 +93,29 @@ public class SnowFallView extends View {
 			canvas.restore();
 		}
 		invalidate();
+	}
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		// TODO Auto-generated method stub
+		switch (event.getAction()) {
+			case MotionEvent.ACTION_UP:
+				RelativeLayout.LayoutParams params;
+				if (flag) {
+					params = (RelativeLayout.LayoutParams)getLayoutParams();
+					params.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+					params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+					int squareLength = parentContext.getResources().getDisplayMetrics().widthPixels;
+					params.width = squareLength / 4;
+					params.height = squareLength / 4;
+				} else {
+					params = new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+				}
+				flag = !flag;
+				setLayoutParams(params);
+				break;
+		}
+		return true;
 	}
 
 }
